8fcc9c726a9df2db3c460bf0ae784dccf3a29a61
Vladimir Bakum <rsoft.vladimir@gmail.com>
2016-09-16 14:17:06 +0300

    Update init.lua

--- ds18b20_thingspeak/init.lua
+++ ds18b20_thingspeak/init.lua
@@ -1,7 +1,9 @@
+-- By (R)soft 15.09.2016 for ESP8266 NodeMcu with ESP-12E
+
 print("Setting up WIFI...")
 wifi.setmode(wifi.STATION)
 --modify according your wireless router settings
-wifi.sta.config("z","Password")
+wifi.sta.config("SSID","Password")
 wifi.sta.connect()
 tmr.alarm(1, 1000, 1, function() 
 if wifi.sta.getip()== nil then 
