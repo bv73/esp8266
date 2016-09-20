34723bf42133a167768a1d73cc0ab503d17fa60a
Vladimir Bakum <rsoft.vladimir@gmail.com>
2016-09-20 14:36:16 +0300

    Add files via upload

--- ds18b20_thingspeak/init.lua
+++ ds18b20_thingspeak/init.lua
@@ -1,16 +1,15 @@
--- By (R)soft 15.09.2016 for ESP8266 NodeMcu with ESP-12E
-
-print("Setting up WIFI...")
+print("Setting up Wi-Fi...")
 wifi.setmode(wifi.STATION)
+
 --modify according your wireless router settings
-wifi.sta.config("SSID","Password")
+wifi.sta.config("z","fGq_a8dWXVZYB219")
 wifi.sta.connect()
 tmr.alarm(1, 1000, 1, function() 
 if wifi.sta.getip()== nil then 
-print("IP unavaiable, Waiting...") 
+   print("IP unavaiable, Waiting...") 
 else 
-tmr.stop(1)
-print("Config done, IP is "..wifi.sta.getip())
-dofile("sender.lua")
+   tmr.stop(1)
+   print("Config done, IP is " .. wifi.sta.getip())
+   dofile("sender.lua")
 end
 end)
--- ds18b20_thingspeak/sender.lua
+++ ds18b20_thingspeak/sender.lua
@@ -1,34 +1,41 @@
 -- By (R)soft 18.09.2016 v1.1
+-- This example require modules 'bit' & '1-wire' in the nodemcu-build.com
 
 pin = 5 -- pin5 = D5 (GPIO14)
+
 require('ds18b20')
 ds18b20.setup(pin)
 
 function sendData()
-t=ds18b20.read()
-print("Temperature:"..t.." C\n")
--- conection to thingspeak.com
-print("Sending data to thingspeak.com")
-conn=net.createConnection(net.TCP, 0) 
-conn:on("receive", function(conn, payload) print(payload) end)
+  t=ds18b20.read()
+  print("Temperature:" .. string.format("%.3f", t) .. " C\n")
 
--- api.thingspeak.com 184.106.153.149
-conn:connect (80,'184.106.153.149')
-conn:send(
-     "GET /update?key=API_KEY&field1=" .. t ..
-     " HTTP/1.1\r\n" ..
-     "Host: api.thingspeak.com\r\n" ..
-     "Accept: */*\r\n" ..
-     "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n" ..
-     "\r\n")
-conn:on("sent",function(conn)
-                   print("Closing connection")
-                   conn:close()
-               end)
-conn:on("disconnection", function(conn)
-                            print("Got disconnection...")
-                         end)
+  -- conection to thingspeak.com
+  conn = net.createConnection(net.TCP, 0) 
+  conn:connect (80,'184.106.153.149')
+  conn:on("connection",
+    function(conn) print("Connected")
+      conn:send('GET /update?key=80AFCACNYJ7AUREP' ..
+      '&field1=' .. string.format("%.3f", t) ..
+      ' HTTP/1.1\r\n' ..
+      'Host: api.thingspeak.com\r\n' ..
+      'Accept: */*\r\n' ..
+      'User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n' ..
+      '\r\n')
+    end)
+  conn:on("sent",function(conn)
+                    print("Data sent")
+                    conn:close()    -- You can disable this row for recieve thingspeak.com answer
+                 end)
+  conn:on("receive",
+     function(conn, payload)
+       print(payload)
+       conn:close()
+     end)
+  conn:on("disconnection", function(conn)
+                              print("Disconnect")
+                           end)
 end
 
--- send data every X ms to thing speak
-tmr.alarm(0, 60000, 1, function() sendData() end )
+  -- send data every X ms to thing speak
+  tmr.alarm(0, 20000, 1, function() sendData() end )
