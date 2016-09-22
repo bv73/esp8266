c3112126f77ad6a1cdfa327faa3b03e92039e347
Vladimir Bakum <rsoft.vladimir@gmail.com>
2016-09-22 12:51:28 +0300

    Update init.lua

--- mpl3115a2/init.lua
+++ mpl3115a2/init.lua
@@ -1,3 +1,7 @@
+-- By (R)soft 22.09.2016 v1.3
+-- This example require module 'i2c' in the nodemcu-build.com
+-- Testing on the binary nodemcu 1.5.4.1
+
 mpl3115a2 = require('mpl3115a2')
 
 id = 0  -- Software I2
