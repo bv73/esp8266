ecaebe02bfce39dcfd66a09b08fde9b4df951df5
Vladimir Bakum <rsoft.vladimir@gmail.com>
2016-09-22 13:08:43 +0300

    Add files via upload

--- mpl3115a2_thingspeak/mpl3115a2.lua
+++ mpl3115a2_thingspeak/mpl3115a2.lua
@@ -1,31 +1,26 @@
--- Library for working with MPL3115A2 I2C Barometric/Altitude sensor
--- As used on breakout board by Adafruit
--- Adapted from code by gareth@l0l.org.uk, Hamish Cunningham and Aeprox@github
--- This code is GPL v3 by steve@wormpurple.com
--- Some modified by (R)soft 16.09.2016 for ESP8266 NodeMcu board
+-- Library for MPL3115A2 I2C Barometric/Altitude sensor
+-- By (R)soft 22.09.2016 v1.3
 
 local moduleName = "mpl3115a2"
 
 local M = { MPL3115A2_ADDR = 0x60 }
-
 _G[moduleName] = M
 
-local busid = 0 -- i2c bus id
+local id = 0 -- i2c bus id
 
 local function read_reg(dev_addr, reg_addr)
      i2c.start(id)
-     a = i2c.address(id, dev_addr ,i2c.TRANSMITTER)
+     a = i2c.address(id, dev_addr, i2c.TRANSMITTER)
 	 if a ~= true then 
-		print("MPL3115A2 not connected")
+		print("Connection Error")
 	end
-     i2c.write(id,reg_addr)
-     -- i2c.stop(id) < Don't send a stop for this device
+     i2c.write(id, reg_addr)
      i2c.start(id)
-     a = i2c.address(id, dev_addr,i2c.RECEIVER)
+     a = i2c.address(id, dev_addr, i2c.RECEIVER)
 	 if a ~= true then 
-		print("MPL3115A2 not connected")
+		print("Connection Error")
 	end
-     c=i2c.read(id,1)
+     c = i2c.read(id, 1)
      i2c.stop(id)
      return string.byte(c)
 end
@@ -34,51 +29,59 @@ local function write_reg(dev_addr, reg_addr, val)
     i2c.start(id)
     a = i2c.address(id, dev_addr ,i2c.TRANSMITTER)
 	if a ~= true then 
-		print("MPL3115A2 not connected")
+		print("Connection Error")
 	end
      c = i2c.write(id,reg_addr, val)
      i2c.stop(id)
 end
 
 local function getData(addr) -- Do the actual reading from the sensor
-    i2c.setup(id,sda,scl,i2c.SLOW)
+  i2c.setup(id, sda, scl, i2c.SLOW)
 	 
-	write_reg(addr,0x26,0x38)
-	write_reg(addr,0x13,0x07)
-	write_reg(addr,0x26,0x39)
+  write_reg(addr, 0x26, 0x3B) -- set OST bit
+  uart.write(0, "<") -- pause
+  write_reg(addr, 0x26, 0x39)
 
-	uart.write(0,"Waiting for data from MPL3115A2")
-	repeat
-		status = read_reg(addr,0x00)
-		-- uart.write(0,".")
-	until bit.band(status,0x08) == 0x08 
+  uart.write(0, "Waiting")
+  repeat
+    status = read_reg(addr, 0x00)
+    uart.write(0, ".")
+  until bit.band(status, 0x08) == 0x08 -- control TPDR bit
 
-	print()
-	
-	local Pmsb = read_reg(addr,0x01)
-	local Pcsb = read_reg(addr,0x02)
-	local Plsb = read_reg(addr,0x03)
-	local Tmsb = read_reg(addr,0x04)
-	local Tlsb = read_reg(addr,0x05)
+  local Pmsb = read_reg(addr, 0x01)
+  local Pcsb = read_reg(addr, 0x02)
+  local Plsb = read_reg(addr, 0x03)
+  local Tmsb = read_reg(addr, 0x04)
+  local Tlsb = read_reg(addr, 0x05)
 
-	local p = bit.lshift(Pmsb,8)
-	p = bit.bor(p, Pcsb)
-	p = bit.lshift(p,8)
-	p = bit.bor(p, Plsb)
-	p = bit.rshift(p,4)
+  write_reg(addr, 0x26 ,0x3B) -- set OST bit
+  uart.write(0, ">") -- pause
+  write_reg(addr, 0x26, 0x39)
+  print()
 
-	local baro = p/4
-    baro = baro/133.3 -- Convert from Pascal to mmHg
+  local p = bit.lshift(Pmsb, 8)
+  p = bit.bor(p, Pcsb)
+  p = bit.lshift(p, 8)
+  p = bit.bor(p, Plsb)
+  p = bit.rshift(p, 4)
+  p = p/4
 	 
-	local t = (Tmsb * 256) + Tlsb
-	t = bit.rshift(t,4)
-	t = t/16.0
+  local t = (Tmsb * 256) + Tlsb
+  t = bit.rshift(t, 4)
+  t = t/16.0
 
-	return baro, t
+  return p, t
+end
+
+function M.init()
+  i2c.setup(id, sda , scl, i2c.SLOW)
+  -- Set Barometer Mode, SampleRate=0x07, SBYB=1
+  write_reg(M.MPL3115A2_ADDR, 0x26, 0x39)
+  -- enable Event Flags
+  write_reg(M.MPL3115A2_ADDR, 0x13, 0x07)
 end
 
 function M.read()
-	i2c.setup(busid, sda , scl, i2c.SLOW)
 	baro,temp = getData(M.MPL3115A2_ADDR)
 	return baro,temp
 end
--- mpl3115a2_thingspeak/sender.lua
+++ mpl3115a2_thingspeak/sender.lua
@@ -1,37 +1,48 @@
--- By (R)soft 16.09.2016 for ESP8266 NodeMcu module with ESP-12E
+-- By (R)soft 22.09.2016 v1.2
+-- This example require module 'i2c' in the nodemcu-build.com
+-- Testing on the binary nodemcu 1.5.4.1
 
 require('mpl3115a2')               
 
 id = 0  -- Software I2C
-
-sda = 3 -- connect to pin GPIO0 (D3)
-scl = 4 -- connect to pin GPIO2 (D4)
+sda = 6 -- sda pin, GPIO12
+scl = 5 -- scl pin, GPIO14
+mpl3115a2.init()
 
 function sendData()
-baro, temp = mpl3115a2.read()
-print(string.format("Barometric pressure: %f mmHg", baro))
-print(string.format("Temperature: %f C", temp))
+  p, t = mpl3115a2.read()
+  p = p/133.3 -- Convert from Pa to mmHg
 
--- conection to thingspeak.com
-print("Sending data to thingspeak.com")
-conn=net.createConnection(net.TCP, 0) 
-conn:on("receive", function(conn, payload) print(payload) end)
+  print("Temperature:" .. string.format("%.3f", t) .. " C")
+  print("Pressure:" .. string.format("%.3f", p) .. " mmHg")
 
--- api.thingspeak.com 52.7.53.111 (old 184.106.153.149)
-conn:connect(80,'52.7.53.111') 
-conn:send("GET /update?key=API_KEY&field1="..temp.."&field2="..baro.."HTTP/1.1\r\n") 
-conn:send("Host: api.thingspeak.com\r\n") 
-conn:send("Accept: */*\r\n") 
-conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
-conn:send("\r\n")
-conn:on("sent",function(conn)
-                      print("Closing connection")
-                      conn:close()
-                  end)
-conn:on("disconnection", function(conn)
-                                print("Got disconnection...")
-  end)
+  -- conection to thingspeak.com
+  conn = net.createConnection(net.TCP, 0) 
+  conn:connect (80,'184.106.153.149')
+  conn:on("connection",
+    function(conn) print("Connected")
+      conn:send('GET /update?key=80AFCACNYJ7AUREP' ..
+      '&field1=' .. string.format("%.3f", t) ..
+      '&field2=' .. string.format("%.3f", p) ..
+      ' HTTP/1.1\r\n' ..
+      'Host: api.thingspeak.com\r\n' ..
+      'Accept: */*\r\n' ..
+      'User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n' ..
+      '\r\n')
+    end)
+  conn:on("sent",function(conn)
+                    print("Data sent")
+                    conn:close()    -- You can disable this row for recieve thingspeak.com answer
+                 end)
+  conn:on("receive",
+     function(conn, payload)
+       print(payload)
+       conn:close()
+     end)
+  conn:on("disconnection", function(conn)
+                              print("Disconnect")
+                           end)
 end
 
--- send data every X ms to thing speak
-tmr.alarm(0, 60000, 1, function() sendData() end )
+  -- send data every X ms to thing speak
+  tmr.alarm(0, 60000, 1, function() sendData() end )
