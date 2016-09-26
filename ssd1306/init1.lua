9f5487f6df2bae66b1c6d74b372feb79df86d634
Vladimir Bakum <rsoft.vladimir@gmail.com>
2016-09-26 15:29:13 +0300

    Add files via upload

---
+++ ssd1306/init1.lua
@@ -0,0 +1,38 @@
+function init_i2c_display()
+     -- SDA and SCL can be assigned freely to available GPIOs
+     local sda = 6
+     local scl = 5
+     local sla = 0x3c
+     i2c.setup(0, sda, scl, i2c.SLOW)
+     disp = u8g.ssd1306_128x64_i2c(sla)
+     disp:setFont(u8g.font_chikita) -- font_chikita font_6x10
+     disp:setFontRefHeightExtendedText()
+     disp:setDefaultForegroundColor()
+     disp:setFontPosTop()
+     --disp:setRot180() 
+end
+
+function write_OLED() -- Write Display
+   disp:firstPage()
+   repeat
+     disp:drawFrame(0, 0, 128, 64)
+     disp:drawStr(2, 0, "BEGIN")
+--     disp:drawStr(35, 30,  string.format("%02d:%02d:%02d",hd,m,s)..meridies)
+     disp:drawStr(2, 6, "NEXT")
+     disp:drawCircle(18, 47, 14)
+     disp:drawBox(70,30,10,10)
+   until disp:nextPage() == false   
+end
+
+function aaa()
+  init_i2c_display()
+
+  disp:firstPage()
+  disp:drawStr(5, 1, "XBM picture")
+  disp:drawBox(0,0,10,10)
+end
+
+init_i2c_display()
+write_OLED()
+
+--tmr.alarm(0, 3000, 1, function() write_OLED() end )
---
+++ ssd1306/init2.lua
@@ -0,0 +1,36 @@
+wifi.setmode(wifi.STATION) --Set mode to STATION so he chip can receive the SSID broadcast
+function init_OLED(sda,scl) --Set up the u8glib lib
+     sla = 0x3c
+     i2c.setup(0, sda, scl, i2c.SLOW)
+     disp = u8g.ssd1306_128x64_i2c(sla)
+     disp:setFont(u8g.font_6x10)
+     disp:setFontRefHeightExtendedText()
+     disp:setDefaultForegroundColor()
+     disp:setFontPosTop()
+end
+init_OLED(6,5) --Run setting up
+
+disp:firstPage()
+repeat
+disp:drawStr(0,0,"ESP8266 Info") -- Starting on line 0
+disp:drawStr(0,11*2,"IP Address:")
+disp:drawStr(0,11*3,wifi.sta.getip())
+disp:drawStr(0,11*4,"MAC Address:")
+disp:drawStr(0,11*5,wifi.sta.getmac()) -- 
+until disp:nextPage() == false
+
+tmr.delay(7000000) --7 second delay
+
+tmr.alarm(0,3000,1,function()
+    wifi.sta.getap(function(t) 
+         disp:firstPage()
+         repeat
+         disp:drawStr(0,0,"WiFi AP Search") -- Starting on line 0
+            lines = 2
+            for k,v in pairs(t) do
+                disp:drawStr(0,lines * 11,k.." "..v:sub(3,5).."dbi")
+                lines = lines + 1
+            end
+        until disp:nextPage() == false
+    end)
+end)
