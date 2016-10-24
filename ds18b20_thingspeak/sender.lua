8c428f65a3bc3d9c9575eb2bd980610a0cb1c00b
Vladimir Bakum <rsoft.vladimir@gmail.com>
2016-10-24 15:03:10 +0300

    Update sender.lua

--- ds18b20_thingspeak/sender.lua
+++ ds18b20_thingspeak/sender.lua
@@ -8,7 +8,7 @@ ds18b20.setup(pin)
 
 function sendData()
   t=ds18b20.read()
-  print("Temperature:" .. string.format("%.3f", t) .. " C\n")
+  print("Temperature :" .. string.format("%.1f", t) .. " C\n")
 
   -- conection to thingspeak.com
   conn = net.createConnection(net.TCP, 0) 
@@ -16,25 +16,27 @@ function sendData()
   conn:on("connection",
     function(conn) print("Connected")
       conn:send('GET /update?key=API_KEY' ..
-      '&field1=' .. string.format("%.3f", t) ..
+      '&field1=' .. string.format("%.1f", t) ..
       ' HTTP/1.1\r\n' ..
       'Host: api.thingspeak.com\r\n' ..
       'Accept: */*\r\n' ..
       'User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n' ..
       '\r\n')
     end)
-  conn:on("sent",function(conn)
-                    print("Data sent")
-                    conn:close()    -- You can disable this row for recieve thingspeak.com answer
-                 end)
+  conn:on("sent",
+    function(conn)
+      print("Data sent")
+      conn:close()    -- You can disable this row for recieve thingspeak.com answer
+    end)
   conn:on("receive",
-     function(conn, payload)
-       print(payload)
-       conn:close()
-     end)
-  conn:on("disconnection", function(conn)
-                              print("Disconnect")
-                           end)
+    function(conn, payload)
+      print(payload)
+      conn:close()
+    end)
+  conn:on("disconnection", 
+    function(conn)
+      print("Disconnect")
+    end)
 end
 
   -- send data every X ms to thing speak
