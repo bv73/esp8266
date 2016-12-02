e015af52018851a16597e5889bb556f7b21dd64d
Vladimir Bakum <rsoft.vladimir@gmail.com>
2016-12-02 15:34:30 +0200

    Add files via upload

--- roboremo_servo/init.lua
+++ roboremo_servo/init.lua
@@ -2,11 +2,12 @@
 -- This example version for NodeMCU firmware with 'pwm' module
 -- Use servo_iface4 for RoboRemo App
 -- Accelerometer via pwm
+-- For MG90S servo set acc. in RoboRemo to 28...128
 
 wifi.setmode(wifi.SOFTAP)
 
 cfg={}
-cfg.ssid="esp_srv"
+cfg.ssid="esp_servo"
 cfg.pwd="12345678"
 
 cfg.ip="192.168.0.1"
@@ -18,6 +19,22 @@ port = 9876
 wifi.ap.setip(cfg)
 wifi.ap.config(cfg)
 
+scl = 5 -- D5 pin (standart for my PCB)
+sda = 6 -- D6 pin
+
+i2c.setup(0, sda, scl, i2c.SLOW)
+
+function read_ads1110 ()
+  i2c.start(0)
+  a = i2c.address(0, 0x48, i2c.RECEIVER)
+  if a ~= true then  print("Connection Error")  end
+  b = i2c.read(0, 2) -- read two bytes
+  i2c.stop(0)
+  c = b:byte(1) * 256 + b:byte(2)
+  return c
+end
+
+
 function stringStarts(a,b)
   return string.sub(a,1,string.len(b))==b
 end
@@ -26,33 +43,67 @@ function stringEnds(a,b)
   return b=='' or string.sub(a,-string.len(b))==b
 end
 
+-- in servo acc. x config: id=servo gain=1.0 min=68 max=82
 servo = {}
-servo.pin = 4 --this is GPIO2
-servo.value = 10 -- Initial value from 0 to 1023
+servo.pin = 8 -- D8
+servo.value = 69 -- 75-6 Initial value from 0 to 1023
 servo.id = "servo"
+
+-- in slider: id=motor min=1023 max=0, send when moved,
+--             auto return, return value=min
+motor = {}
+motor.pinA = 1 -- D1 pin
+motor.pinB = 2 -- D2 pin
+motor.reverse = 0
+motor.value = 1023
+motor.id = "motor"
 
 cmd = ""
 
-pwm.setup(servo.pin, 50, 10) -- 50 Hz, Initial value=10
+pwm.setup(servo.pin, 50, 75-6) -- 50 Hz, Initial value=10
 pwm.start(servo.pin)
-
--- setup LED pin
-led = 5
-gpio.mode(led, gpio.OUTPUT)
-gpio.write(led, gpio.HIGH) -- LED turn off
+pwm.setup(motor.pinA, 50, 1023) -- 50 Hz, Initial value=1023
+pwm.setup(motor.pinB, 50, 1023)
+pwm.start(motor.pinA)
+pwm.start(motor.pinB)
     
 -- servo value from 0 to 1023
 function exeCmd(st) -- example: "servo 500" or "led1"
   if stringStarts(st, servo.id.." ") then -- value comes after id + space
     servo.value = tonumber( string.sub(st,1+string.len(servo.id.." "),string.len(st)) )
     -- set pwm after get value
-    pwm.setduty(servo.pin, servo.value)
+    pwm.setduty(servo.pin, servo.value-6)
+    tmp = read_ads1110()
+    volt = tmp*1.279e-4
     -- send back to RoboRemo of value for monitoring
-    connection:send("back " .. servo.value .. "\n")
-  elseif stringStarts(st, "led0") then
-    gpio.write(led, gpio.HIGH)
-  elseif stringStarts(st, "led1") then
-    gpio.write(led, gpio.LOW) -- LED on
+    -- in level indicators for voltage & servo values set:
+    -- id=volt label: #*1.0 & id=back label: #*1.0
+    -- and set min & max
+    connection:send("back " .. servo.value .. 
+    "\nvolt " .. string.format("%.1f", volt) ..     "\n")
+--    print(string.format("volt %.1f", volt))
+  elseif stringStarts(st, motor.id.." ") then -- value comes after id + space
+    motor.value = tonumber( string.sub(st,1+string.len(motor.id.." "),string.len(st)) )
+    -- set pwm after get value
+    if (motor.reverse == 0) then
+--      print("frw=" .. motor.value)
+      pwm.setduty(motor.pinA, motor.value)
+      pwm.setduty(motor.pinB, 1023)
+      else
+--      print("bck=" .. motor.value)
+      pwm.setduty(motor.pinB, motor.value)
+      pwm.setduty(motor.pinA, 1023)
+      end
+  elseif stringStarts(st, "rev0") then
+--    print("rev0")
+    motor.reverse = 0
+    pwm.setduty(motor.pinA, motor.value)
+    pwm.setduty(motor.pinB, 1023)
+  elseif stringStarts(st, "rev1") then
+--    print("rev1")
+    motor.reverse = 1 -- REVerse on
+    pwm.setduty(motor.pinB, motor.value)
+    pwm.setduty(motor.pinA, 1023)
   end
 end
 
